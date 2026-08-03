f916c24ed1714ab06f36f78f496bfc858b73efb1
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-08-03 20:13:59 +0530

    feat: implement MongoDB connection and add mongoose as a dependency

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import { connectDB } from "./config/db.js";
 
-dotenv.config({ path: "backend/config/config.env" });
-const port=process.env.PORT || 3000;
-
+dotenv.config({ path: "./config/config.env" });
+connectDB();
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`server is running  on  port ${port}.`);
