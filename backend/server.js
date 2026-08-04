9f847c397c4be5609d83213d058d51bc5951e478
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-08-04 16:50:22 +0530

    feat(server): enhance error handling with uncaught exceptions and promise rejections

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,28 @@ import dotenv from "dotenv";
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
 
+// 1. Handling Uncaught Exception (Pehle top par rakhna zaroori hai)
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting down the server due to Uncaught Exception");
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config/config.env" });
 connectDB();
+
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`server is running  on  port ${port}.`);
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
+
+// 2. Handling Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting down the server due to Unhandled Promise Rejection");
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
