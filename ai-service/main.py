9b9587ece5aefccf14b2c0c5ec28f3680584f2f8
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-09-05 20:16:59 +0530

    feat: Add title to FastAPI application and clean up whitespace in main.py

--- ai-service/main.py
+++ ai-service/main.py
@@ -4,7 +4,8 @@ from config.db import products_collection
 from services.ai_service import ask_ai
 from services.embedding_service import generate_embedding
 from services.semantic_search import semantic_search_products
-app = FastAPI()
+
+app = FastAPI(title="NexusCart AI Service")
 
 
 class ChatRequest(BaseModel):
@@ -21,21 +22,17 @@ def home():
 
 @app.post("/ai/chat")
 def ai_chat(data: ChatRequest):
-
     answer = ask_ai(data.message)
-
     return {
         "success": True,
         "message": answer
     }
 
+
 @app.get("/test-embedding")
 def test_embedding():
-
     text = "Mechanical RGB Gaming Keyboard"
-
     vector = generate_embedding(text)
-
     return {
         "success": True,
         "text": text,
@@ -43,13 +40,10 @@ def test_embedding():
         "vector": vector
     }
 
-    
 
 @app.get("/semantic-search")
 def semantic_search(query: str):
-
     results = semantic_search_products(query)
-
     return {
         "success": True,
         "query": query,
